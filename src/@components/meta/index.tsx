5b7858d340b1ec505fdd77d521f3dbca6888845b
duongpham17 <duongpham17@icloud.com>
2023-04-05 00:26:25 +0100

    image

--- src/@components/meta/index.tsx
+++ src/@components/meta/index.tsx
@@ -23,7 +23,7 @@ export const Layout = ({title, image, ogTitle = defaultOgTitle, description = de
         <meta name="og:title" content={ogTitle} key="og:title" />
         <meta property="og:url" content="https://www.wendynail.co.uk" key="og:url"/>
         <meta property="og:type" content="website" key="og:type"/>
-        <meta property="og:image" content={image? "/skin1.png" : image} key="ogimage"/>
+        <meta property="og:image" content={image} key="ogimage"/>
         <meta property="og:description" content={description} />
         <meta name="keywords" content={keywords} />
     </Head>
