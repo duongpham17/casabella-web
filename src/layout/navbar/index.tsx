b30c0f228d3ac12887d20158533eca4cc96c30e9
duongpham17 <duongpham17@icloud.com>
2023-08-07 01:39:45 +0100

    scaled navbar

--- src/layout/navbar/index.tsx
+++ src/layout/navbar/index.tsx
@@ -13,7 +13,7 @@ const Index = () => {
     return (
         <div className={styles.container}>
 
-            { width >= 800  
+            { width >= 900  
                 ?
                     <Large />
                 :
