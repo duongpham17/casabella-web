a8629f41bf6e3b6461c79ea3cfb836dc3dda9cb7
duongpham17 <duongpham17@icloud.com>
2023-08-23 23:34:25 +0100

    intro

--- src/routes/home/introduction/index.tsx
+++ src/routes/home/introduction/index.tsx
@@ -8,15 +8,21 @@ import Link from '@components/link/Style1';
 const Introduction = () => {
   return (
     <div className={styles.container}>
-        <section>
 
-            <Header value="Where Beauty and Elegance meet" size='clamp(4vw, 5vw, 6vw)' center/>
+      <video autoPlay muted loop id="myVideo">
+        <source src="/intro.mp4" type="video/mp4" />
+      </video>
 
-            <Text value="Step into a realm of indulgence where we celebrate and enhance your distinct radiance." center/>
-            
-            <Link value="book now" href="http://casabella.zenoti.com/webstorenew" open padding='1rem 2rem'/>
-            
-        </section>
+      <section>
+
+          <Header value="Where Beauty and Elegance meet" size='clamp(4vw, 5vw, 6vw)' center/>
+
+          <Text value="Step into a realm of indulgence where we celebrate and enhance your distinct radiance." center/>
+          
+          <Link value="book now" href="http://casabella.zenoti.com/webstorenew" open padding='1rem 2rem'/>
+          
+      </section>
+        
     </div>
   )
 }
